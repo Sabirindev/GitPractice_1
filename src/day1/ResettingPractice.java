eb3abaedaaf0811236a3e92296e117997001c0a1
sabir mustafayev <sdetinscrum@gmail.com>
2021-07-26 12:52:51 -0400

    Test case is all done

--- src/day1/ResettingPractice.java
+++ src/day1/ResettingPractice.java
@@ -5,5 +5,9 @@ public class ResettingPractice {
         // Test case part1 is done
         // Test case part2 is done
         // Test case part3 is done
+        // Test case part4 is done
+        // Test case part5 is done
+        //Mixed/Soft reset remove the commit from the history but keep the local changes
+        // Did part 4 par 5 and I am done in one commit
     }
 }
