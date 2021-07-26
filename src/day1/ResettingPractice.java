028c7fb6e745601545fa204850469fcee46eac60
sabir mustafayev <sdetinscrum@gmail.com>
2021-07-26 12:37:37 -0400

    Test case part3 is done

--- src/day1/ResettingPractice.java
+++ src/day1/ResettingPractice.java
@@ -4,5 +4,6 @@ public class ResettingPractice {
     public static void main(String[] args) {
         // Test case part1 is done
         // Test case part2 is done
+        // Test case part3 is done
     }
 }
