3a47cfea9d602a5e911f1ab0c802a092c9d11783
sabir mustafayev <sdetinscrum@gmail.com>
2021-07-29 14:47:06 -0400

    Set is cool can't wait to learn

--- src/day2/SetPractice.java
+++ src/day2/SetPractice.java
@@ -3,4 +3,5 @@ package day2;
 public class SetPractice {
     //Set is a collection type that only store unique item
     //if you add dupicate it will just remove it
+    //it's a collection of unique elements
 }
