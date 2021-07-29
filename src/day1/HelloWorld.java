04e119b88ac00b17c250f326e28d4d466546591f
sabir mustafayev <sdetinscrum@gmail.com>
2021-07-29 14:21:04 -0400

    added HelloWorld.java file

--- src/day1/HelloWorld.java
+++ src/day1/HelloWorld.java
@@ -1,7 +1,21 @@
 package day1;
 
+import java.util.Scanner;
+
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("hello worlD");
+
+        Scanner scan = new Scanner(System.in);
+        String b = scan.next();
+
+        scan.nextLine();
+
+        String a = scan.nextLine();
+
+
+        System.out.println(a);
+        System.out.println(b);
     }
+
 }
