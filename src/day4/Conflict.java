c47ffa989ad1f3c9517c7dec9a563c6661420750
sabir mustafayev <sdetinscrum@gmail.com>
2021-07-30 10:45:56 -0400

    change coming from collection2 branch

--- src/day4/Conflict.java
+++ src/day4/Conflict.java
@@ -3,4 +3,5 @@ package day4;
 public class Conflict {
     //commit2 from loacl Master branch
     //commit 1 from local from collection branch
+    // This is change coming from collection2 branch
 }
