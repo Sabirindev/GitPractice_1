27d15b82c88fecd4e8d6e2029b6aa3790f5c92bb
sabir mustafayev <sdetinscrum@gmail.com>
2021-07-30 10:47:14 -0400

    change in master Branch

--- src/day4/Conflict.java
+++ src/day4/Conflict.java
@@ -3,4 +3,5 @@ package day4;
 public class Conflict {
     //commit2 from loacl Master branch
     //commit 1 from local from collection branch
+    //this is the change in master branch that collection2 branch does not know about
 }
